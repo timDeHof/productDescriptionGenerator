673aca28a446993d0026f54e0b7c2640faa78f4c
timDeHof <this.is.tim.dehof@gmail.com>
2022-05-18 14:12:45 -0400

    fixed head page to have default export

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env["OPENAI_API_KEY"],
 });
 const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
--- pages/head.js
+++ pages/head.js
@@ -2,17 +2,19 @@ import * as React from "react";
 import NextHead from "next/head";
 import { GoogleFonts } from "next-google-fonts";
 
-export const Head = ({ children, title }) => (
-  <>
-    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" />
-    <NextHead>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+export default function Head({ children, title }) {
+  return (
+    <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" />
+      <NextHead>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
 
-      <title>{title}</title>
+        <title>{title}</title>
 
-      {children}
-    </NextHead>
-  </>
-);
+        {children}
+      </NextHead>
+    </>
+  );
+}
